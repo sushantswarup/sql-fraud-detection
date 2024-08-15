46610e820f456c64d7fae854e8255992d79367f1
sushant swarup <116436039+sushantswarup@users.noreply.github.com>
2024-08-15 16:42:12 +0530

    Update schema.sql

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,6 +1,6 @@
 ﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/s8Wnqm
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
 
 --Card Holder
 
